ebb6d8528ab35fe44f952d4f696189b1e8f499f5
kenneth34 <kendor34@yahoo.com.sg>
2022-02-12 16:56:36 +0800

    basic done

--- screens/AccountScreen.js
+++ screens/AccountScreen.js
@@ -1,13 +1,70 @@
 import React from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { useState, useEffect } from "react";
+import {
+  ActivityIndicator,
+  Button,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import { commonStyles } from "../styles/commonStyles";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+
+const API = "https://kennethwong.pythonanywhere.com";
+const API_WHOAMI = "/whoami";
 
 export default function AccountScreen({ navigation }) {
-  function signOut() {}
+
+  const [username, setUsername] = useState("");
+
+  async function getUsername() {
+    console.log("---- Getting user name ----");
+    const token = await AsyncStorage.getItem("token");
+    console.log(`Token is ${token}`);
+    try {
+      const response = await axios.get(API + API_WHOAMI, {
+        headers: { Authorization: `JWT ${token}` },
+      });
+      console.log("Got user name!");
+      //console.log(response);
+      setUsername(response.data.username);
+    } catch (error) {
+      console.log("Error getting user name");
+      if (error.response) {
+        console.log(error.response.data);
+        if (error.response.data.status_code === 401) {
+          signOut();
+        }
+      } else {
+        console.log(error);
+      }
+    }
+  }
+
+  useEffect(() => {
+    console.log("Setting up nav listener");
+    // Check for when we come back to this screen
+    const removeListener = navigation.addListener("focus", () => {
+      console.log("Running nav listener");
+      //setUsername("");
+      setUsername(<ActivityIndicator />);
+      getUsername();
+    });
+    getUsername();
+
+    return removeListener;
+  }, []);
+
+  function signOut() {
+    AsyncStorage.removeItem("token");
+    navigation.navigate("SignIn");
+  }
 
   return (
     <View style={commonStyles.container}>
       <Text>Account Screen</Text>
+      <Text>{username}</Text>
       <Button title="Sign out" onPress={signOut} />
     </View>
   );
--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -8,16 +8,47 @@ import {
   TouchableOpacity,
   TouchableWithoutFeedback,
   Keyboard,
+  ActivityIndicator,
 } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+
+const API = "https://kennethwong.pythonanywhere.com";
+const API_LOGIN = "/auth";
 
 export default function SignInScreen({ navigation }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [errorText, setErrorText] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  function login() {
+  async function login() {
+    console.log("---- Login time ----");
     Keyboard.dismiss();
     // do stuff here to log in
+    //AsyncStorage.setItem("token", "demo_token");
+    //navigation.navigate("Account");
+
+    try {
+      setLoading(true);
+      const response = await axios.post(API + API_LOGIN, {
+        username,
+        password,
+      });
+      console.log("Success logging in!");
+      console.log(response);
+
+      AsyncStorage.setItem("token", response.data.access_token);
+      //console.log(response);
+      await AsyncStorage.setItem("token", response.data.access_token);
+      setLoading(false);
+      navigation.navigate("Account");
+    } catch (error) {
+      console.log("Error logging in!");
+      console.log(error.response);
+
+      setErrorText(error.response.data.description);
+    }
   }
 
   return (
@@ -42,10 +73,16 @@ export default function SignInScreen({ navigation }) {
           value={password}
           onChangeText={(input) => setPassword(input)}
         />
-        <TouchableOpacity onPress={login} style={styles.loginButton}>
-          <Text style={styles.buttonText}>Log in</Text>
-        </TouchableOpacity>
+        <View style={{ flexDirection: "row" }}>
+          <TouchableOpacity onPress={login} style={styles.loginButton}>
+            <Text style={styles.buttonText}>Log in</Text>
+          </TouchableOpacity>
+          {loading ? (
+            <ActivityIndicator style={{ marginLeft: 20, marginBottom: 20 }} /> // adjust
+          ) : null}
+        </View>
         <Text style={styles.errorText}>{errorText}</Text>
+        <View style={{ height: 20, alignItems: "left" }}></View>
       </View>
     </TouchableWithoutFeedback>
   );
